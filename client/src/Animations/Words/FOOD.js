fd581ffe6bd7b9552f293e6be90a8b65121fc585
Manishka1 <manishka110518@gmail.com>
2026-04-05 09:09:55 +0530

    FOOD code fix

--- client/src/Animations/Words/FOOD.js
+++ client/src/Animations/Words/FOOD.js
@@ -2,68 +2,35 @@ export const FOOD = (ref) => {
 
     let animations = []
 
-    // RIGHT HAND — claw shape
-    animations.push(["mixamorigRightHandIndex1", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandIndex2", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandIndex3", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandMiddle1", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandMiddle2", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandMiddle3", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandRing1", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandRing2", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandRing3", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandPinky1", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandPinky2", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandPinky3", "rotation", "z", Math.PI/3, "+"]);
-    animations.push(["mixamorigRightHandThumb2", "rotation", "y", -Math.PI/3, "-"]);
-    animations.push(["mixamorigRightHandThumb3", "rotation", "y", -Math.PI/3, "-"]);
+    // RIGHT HAND — cone/pinch shape (like A's right hand)
+    animations.push(["mixamorigRightHandMiddle1", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandMiddle2", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandMiddle3", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandRing1", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandRing2", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandRing3", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandPinky1", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandPinky2", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandPinky3", "rotation", "z", Math.PI/2, "+"]);
+    animations.push(["mixamorigRightHandThumb2", "rotation", "y", -Math.PI/2.5, "-"]);
+    animations.push(["mixamorigRightHandThumb3", "rotation", "y", -Math.PI/2.5, "-"]);
 
-    // RIGHT ARM — bring forward like YOU but less
-    animations.push(["mixamorigRightArm", "rotation", "x", -Math.PI/8, "-"]);
+    // RIGHT ARM — raise up toward face level
+    animations.push(["mixamorigRightArm", "rotation", "x", -Math.PI/4, "-"]);
+    animations.push(["mixamorigRightArm", "rotation", "y", -Math.PI/12, "-"]);
 
-    // RIGHT WRIST — palm facing inward/body
-    animations.push(["mixamorigRightHand", "rotation", "x", Math.PI/6, "+"]);
-    animations.push(["mixamorigRightHand", "rotation", "z", Math.PI/4, "+"]);
-    animations.push(["mixamorigRightHand", "rotation", "y", -Math.PI/8, "-"]);
+    // RIGHT FOREARM — bend to bring hand toward mouth
+    animations.push(["mixamorigRightForeArm", "rotation", "x", -Math.PI/3, "-"]);
 
-    // RIGHT FOREARM — slight bend inward
-    animations.push(["mixamorigRightForeArm", "rotation", "x", Math.PI/10, "+"]);
-
-    // LEFT HAND — mirror claw shape
-    animations.push(["mixamorigLeftHandIndex1", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandIndex2", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandIndex3", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandMiddle1", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandMiddle2", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandMiddle3", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandRing1", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandRing2", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandRing3", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandPinky1", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandPinky2", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandPinky3", "rotation", "z", -Math.PI/3, "-"]);
-    animations.push(["mixamorigLeftHandThumb2", "rotation", "y", Math.PI/3, "+"]);
-    animations.push(["mixamorigLeftHandThumb3", "rotation", "y", Math.PI/3, "+"]);
-
-    // LEFT ARM — mirror of right
-    animations.push(["mixamorigLeftArm", "rotation", "x", -Math.PI/8, "-"]);
-
-    // LEFT WRIST — mirror of right
-    animations.push(["mixamorigLeftHand", "rotation", "x", Math.PI/6, "+"]);
-    animations.push(["mixamorigLeftHand", "rotation", "z", -Math.PI/4, "-"]);
-    animations.push(["mixamorigLeftHand", "rotation", "y", Math.PI/8, "+"]);
-
-    // LEFT FOREARM — mirror
-    animations.push(["mixamorigLeftForeArm", "rotation", "x", Math.PI/10, "+"]);
+    // RIGHT WRIST — orient fingers toward mouth
+    animations.push(["mixamorigRightHand", "rotation", "x", -Math.PI/6, "-"]);
+    animations.push(["mixamorigRightHand", "rotation", "z", Math.PI/8, "+"]);
 
     ref.animations.push(animations);
 
     animations = []
 
     // RIGHT reset
-    animations.push(["mixamorigRightHandIndex1", "rotation", "z", 0, "-"]);
-    animations.push(["mixamorigRightHandIndex2", "rotation", "z", 0, "-"]);
-    animations.push(["mixamorigRightHandIndex3", "rotation", "z", 0, "-"]);
     animations.push(["mixamorigRightHandMiddle1", "rotation", "z", 0, "-"]);
     animations.push(["mixamorigRightHandMiddle2", "rotation", "z", 0, "-"]);
     animations.push(["mixamorigRightHandMiddle3", "rotation", "z", 0, "-"]);
@@ -77,36 +44,12 @@ export const FOOD = (ref) => {
     animations.push(["mixamorigRightHandThumb3", "rotation", "y", 0, "+"]);
 
     animations.push(["mixamorigRightArm", "rotation", "x", 0, "+"]);
+    animations.push(["mixamorigRightArm", "rotation", "y", 0, "+"]);
 
-    animations.push(["mixamorigRightHand", "rotation", "x", 0, "-"]);
+    animations.push(["mixamorigRightForeArm", "rotation", "x", 0, "+"]);
+
+    animations.push(["mixamorigRightHand", "rotation", "x", 0, "+"]);
     animations.push(["mixamorigRightHand", "rotation", "z", 0, "-"]);
-    animations.push(["mixamorigRightHand", "rotation", "y", 0, "+"]);
-
-    animations.push(["mixamorigRightForeArm", "rotation", "x", 0, "-"]);
-
-    // LEFT reset
-    animations.push(["mixamorigLeftHandIndex1", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandIndex2", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandIndex3", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandMiddle1", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandMiddle2", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandMiddle3", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandRing1", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandRing2", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandRing3", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandPinky1", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandPinky2", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandPinky3", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHandThumb2", "rotation", "y", 0, "-"]);
-    animations.push(["mixamorigLeftHandThumb3", "rotation", "y", 0, "-"]);
-
-    animations.push(["mixamorigLeftArm", "rotation", "x", 0, "+"]);
-
-    animations.push(["mixamorigLeftHand", "rotation", "x", 0, "-"]);
-    animations.push(["mixamorigLeftHand", "rotation", "z", 0, "+"]);
-    animations.push(["mixamorigLeftHand", "rotation", "y", 0, "-"]);
-
-    animations.push(["mixamorigLeftForeArm", "rotation", "x", 0, "-"]);
 
     ref.animations.push(animations);
 
